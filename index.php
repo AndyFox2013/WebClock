90ec6a913b9e0d2cc02c4a6df307b8f63edb7e8f
Rinse M. <therinsem@hotmail.com>
2014-01-13 16:06:31 +0100

    "Initial Spark/commit"

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head><title>Clock</title></head>
+<body>
+<script type="text/javascript" src="time.js"></script>
+    <span id="date_time"></span>
+    <script type="text/javascript">window.onload = date_time('date_time');</script>
+</body>
+</html>
